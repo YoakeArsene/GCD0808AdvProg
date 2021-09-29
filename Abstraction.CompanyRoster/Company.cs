d52a2a44b3b7a940caf81973b6192e6eae120195
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-09-29 16:31:27 +0700

    Problem 6. Company Roster (unfinished)

---
+++ Abstraction.CompanyRoster/Company.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Abstraction.CompanyRoster
+{
+  class Company
+  {
+    public List<Employee> Employees;
+
+    public Company()
+    {
+      Employees = new List<Employee>();
+    }
+
+    public decimal GetHighestAverageSalary()
+    {
+      return 0;
+    }
+
+    public string GetDepartmentWithHighestAverageSalary()
+    {
+      return "";
+    }
+
+    public void PrintEmployees(string department)
+    {
+      var employees = Employees.Where(
+        t => t.Department.Equals(department)).ToList();
+      foreach (var employee in employees)
+      {
+        employee.PrintInfo();
+      }
+    }
+  }
+}
---
+++ Abstraction.CompanyRoster/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Abstraction.CompanyRoster
+{
+  class Employee
+  {
+    public string Name;
+    public decimal Salary;
+    public string Position;
+    public string Department;
+    public string Email;
+    public int Age;
+
+    public Employee()
+    {
+    }
+
+    public Employee(string name, decimal salary, string position, string department)
+    {
+      Name = name;
+      Salary = salary;
+      Position = position;
+      Department = department;
+    }
+
+    public Employee(string name, decimal salary, string position, string department,
+      string email, int age)
+    {
+      Name = name;
+      Salary = salary;
+      Position = position;
+      Department = department;
+      Email = email;
+      Age = age;
+    }
+
+    public void PrintInfo()
+    {
+      if (Age != 0)
+      {
+        Console.WriteLine($"{Name} {Salary} {Email} {Age}");
+      }
+      else
+      {
+        Console.WriteLine($"{Name} {Salary} {Email} -1");
+      }
+    }
+  }
+}

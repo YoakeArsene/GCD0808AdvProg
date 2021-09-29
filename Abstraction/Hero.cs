ba57c983910e1ccefc36720866deb5017ffbc136
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-09-29 13:45:19 +0700

    init class Hero

---
+++ Abstraction/Hero.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Abstraction
+{
+  class Hero
+  {
+    // Properties
+    public string FullName;
+    public int HP;
+    public int Mana;
+    public int AttackPoint;
+    public int Level;
+
+
+    // Function
+    public void Walk()
+    {
+      Console.WriteLine("Hero is walking ...");
+    }
+
+    public void Attack()
+    {
+      Console.WriteLine("Hero is attacking ...");
+    }
+
+    public void Jump()
+    {
+      Console.WriteLine("Hero is jumping ...");
+    }
+
+  }
+}
--- Abstraction/Program.cs
+++ Abstraction/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Abstraction
 {
@@ -10,6 +6,29 @@ namespace Abstraction
   {
     static void Main(string[] args)
     {
+      // Use keyword "new" to create object from class
+      Hero hero = new Hero();
+      Hero amazon = new Hero();
+      Hero paladin = new Hero();
+
+      // To access function or property of object, use "."
+      hero.FullName = "Illidan";
+      hero.HP = 1000;
+      hero.Level = 5;
+      hero.Mana = 100;
+      hero.AttackPoint = 150;
+
+      Console.WriteLine(hero.FullName);
+      Console.WriteLine(hero.HP);
+      Console.WriteLine(hero.Mana);
+      Console.WriteLine(hero.Level);
+      Console.WriteLine(hero.AttackPoint);
+
+      hero.Attack();
+      hero.Jump();
+      hero.Walk();
+
     }
+
   }
 }
